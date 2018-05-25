df7202332e30fa116d784d4c4d715b9de2ccf056
Alec Keller <alec.keller.13@cnu.edu>
2018-05-24 23:13:59 -0400

    added console log

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const express = require('express')
 
 const app = express()
 
-app.get('/', (req, res) => res.send("This is the bot server"))
+app.get('/', (req, res) {
+  res.send("This is the bot server");
+  console.log(req);
+});
 
 const PORT = process.env.PORT || 5000
 
