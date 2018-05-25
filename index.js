347c4bb7bbc102c2e35cce3883856fc778d951b8
Alec Keller <alec.keller.13@cnu.edu>
2018-05-24 23:06:58 -0400

    added gitignore and response from server

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 
 const app = express()
 
-app.get('/', (req, res) => console.log(req))
+app.get('/', (req, res) => res.send("This is the bot server"))
 
 const PORT = process.env.PORT || 5000
 
