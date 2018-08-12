3a479a846e4eb7aad4e59e119075d21c45483f10
Alec Keller <alec.keller.13@cnu.edu>
2018-08-12 16:35:27 -0400

    added bad response

--- bot.js
+++ bot.js
@@ -1,8 +1,10 @@
-var request, botID, botResponse, topTiersUrl, pictures, colinFellas, oneLinerJoke, bio, coolText, email, poo, randomResponses, commands
+ar request, botID, botResponse, topTiersUrl, pictures, colinFellas, oneLinerJoke, bio, coolText, email, poo, randomResponses, commands,
+curses
 
 request      = require('request');
 oneLinerJoke = require('one-liner-joke');
 poo          = require('poo');
+curses       = require('curse-words')
 pictures     = require('./pictures.js');
 google       = require('./google.js');
 coolText     = require('./cool-text.js');
@@ -20,8 +22,7 @@ bio          = "Born on December 22, 1970, I grew up in Houston, Texas, earning 
 "presidential campaign of George W. Bush. In 2012 I won election to the U.S. Senate with the support of the Tea Party and took office the" +
 "following year, going on to orchestrate a governmental shutdown in opposition to Obamacare. In 2015, I announced that I was running for"  +
 "the 2016 Republican presidential nomination."
-randomResponses    = ["Not gonna lie, have no clue what to say to you right now. Sry.","Can't talk right now, too busy getin money","lol","Idk what to say so haha",
-"Why don't you try googling it","I'm Ted Cruz and I approve that message","Yeah you and what army","Warriors in 5, nick sucks","Shhhhhh","yeah u thick","ur a good guy","I don't care","ok","suck it","ur mom","no"];
+randomResponses    = ["","Can't talk right now, too busy getin money","lol","Idk what to say so haha","Shhhhhh","yeah u thick","ur a good guy","I don't care","ok","suck it","ur mom","no"];
 commands = "Here are the following commands you can use with me: \n top tiers \n puppy pic \n meme \n gif \n youtube \n" +
 "dominos \n roll dice \n poo fact \n joke \n who are/made you \n who am i \n learn \n say \n like & dislike \n google \n cool text \n and other hidden commands";
 
@@ -164,7 +165,13 @@ function determineResponse(message,sender,messageId) {
         sendMessage("idk but you\'re Uncle B");
       }
       else{
-        sendMessage(randomResponses[randomNum],"");
+        if (randomNum == 0){
+            word = curses()
+            sendMessage("ur a " + word + ", " + sender,"");
+        }
+        else{
+          sendMessage(randomResponses[randomNum],"");
+        }
       }
     }
   }
